98a6abb254054e3472d43a29895e633c4f6f3934
root <mtrhoades0727@gmail.com>
2022-01-20 13:57:19 -1000

    finished assignment

--- index.js
+++ index.js
@@ -2,8 +2,75 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
 
+function handleDirectionChange(direction) {
+if(direction === null){
+    character.src = 'assets/green-character/static.gif'
+}
+if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'
+}
+if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'
+}
+if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'
+}
+if(direction === 'south'){
+    character.src = 'assets/green-character/south.gif'
+}
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+
+// let direction = null;
+// let x = 100;
+// let y = 250;
+
+
+// making the character move with arrow keys
+// function moveCharacter() {
+
+// if(direction === 'west') {
+//     x = x - 1
+// }
+// if(direction === 'north') {
+//     y = y + 1
+// }
+// if(direction === 'east') {
+//     x = x + 1
+// }
+// if(direction === 'south') {
+//     y = y - 1
+// }
+// character.style.left = x + 'px'
+// character.style.bottom = y + 'px'
+// }
+
+// setInterval(moveCharacter, 1)
+
+// document.addEventListener('keydown', function(e) {
+//     if(e.repeat) return;
+    
+//     if(e.key === 'ArrowLeft') {
+//         direction = 'west'
+//     }
+//     if(e.key === 'ArrowUp') {
+//         direction = 'north'
+//     }
+//     if(e.key === 'ArrowRight') {
+//         direction = 'east'
+//     }
+//     if(e.key === "ArrowDown") {
+//         direction = 'south'
+//     }
+// })
+
+// // moving the character with arrow keys one key at a time
+// document.addEventListener('keyup', function(e) {
+//     direction = null
+// })
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
